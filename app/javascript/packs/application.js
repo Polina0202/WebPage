e112dc8848f0a4cfddfea3cdce4d4edcb6c401d6
Vladimir Peskov <vpeskov@niomed.ru>
2021-09-07 17:50:27 +0700

    Добавили bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery"
 import 'bootstrap'
+import '../stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.jQuery = $;
+window.$ = $;
